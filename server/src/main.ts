37d97686bba4453a51fdbf66cdb7f7563af436cf
nguyenngochuy00 <ngochuy25120@gmail.com>
2026-04-27 01:45:36 +0700

    Update server

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,12 +9,20 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 4000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  // Chỉ lắng nghe cổng nếu không phải môi trường Vercel
+  if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 4000;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}`);
+  }
+
+  await app.init();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return app.getHttpAdapter().getInstance();
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+
+export default bootstrap();
